600f9a34c0d118e786f72060e42ab644ca0323f6
coreyborad <coreyborad@hotmail.com.tw>
2018-05-16 12:06:24 +0800

    [Master] 2018-05-16-5

--- web/index.php
+++ web/index.php
@@ -1,12 +1,24 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require __DIR__ . '/../vendor/autoload.php';
 use \Core\Parser AS Parser;
 use Slim\App;
-$app->get('/', function ($request, $response, $args) use ($app) {
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+$c = new \Slim\Container($configuration);
+$app = new \Slim\App($c);
+$app->get('/getlist', function ($request, $response, $args) use ($app) {
     $PttParser = new Parser\Ptt\Main();
     $total_image_url = $PttParser->_Start();
-    $newResponse = $response->withJson($total_image_url);
-    $newResponse = $response->withHeader('Content-type', 'application/json');
-    return $newResponse;
+    return $response->withJSON(
+        $total_image_url,
+        200,
+        JSON_UNESCAPED_UNICODE
+    );
 });
 $app->run();
